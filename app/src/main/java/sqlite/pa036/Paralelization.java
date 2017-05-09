9542da9699ae2982031911e5441cbd9e41a4df6f
Jakub Beer <beerjakubpd@gmail.com>
2017-05-09 19:37:48 +0200

    Paralelization final

--- app/src/main/java/sqlite/pa036/Paralelization.java
+++ app/src/main/java/sqlite/pa036/Paralelization.java
@@ -17,40 +17,107 @@ public final class Paralelization extends DBHelper {
     private TextView textOut;
     private List<String> selects = new ArrayList<String>();
     private List<String> updates = new ArrayList<String>();
-    private int NUM_QUERIES = 500;
+    private int NUM_QUERIES = 3600;
     private int COUNT = 10;
 
     private class first implements Runnable{
-
+        private int numOps;
+        public first(int num)
+        {
+            numOps=num;
+            totalTime=0;
+        }
+        public long totalTime;
         @Override
         public void run() {
             int idx = 0;
-            for(int i = 0; i< NUM_QUERIES; i++)
+            long startTime;
+            long endTime;
+            long time =0;
+            for(int i = 0; i< numOps; i++)
             {
+                startTime = System.currentTimeMillis();
                 Cursor myCurs = myDataBase.rawQuery(selects.get(idx), null);
                 myCurs.moveToFirst();
                 myCurs.close();
+                endTime = System.currentTimeMillis();
+                time += (endTime-startTime);
                 idx++;
                 if(idx ==selects.size())
                     idx = 0;
             }
+            totalTime=time;
         }
     }
 
     private class second implements Runnable{
 
+        private int numOps;
+        public second(int num)
+        {
+            numOps=num;
+            totalTime=0;
+        }
+        public long totalTime;
         @Override
         public void run() {
             int idx = 0;
-            for(int i = 0; i< NUM_QUERIES; i++)
+            long startTime;
+            long endTime;
+            long time = 0;
+            for(int i = 0; i< numOps; i++)
             {
+                startTime = System.currentTimeMillis();
                 Cursor myCurs =myDataBase.rawQuery(updates.get(idx), null);
                 myCurs.moveToFirst();
                 myCurs.close();
+                endTime = System.currentTimeMillis();
+                time += (endTime-startTime);
                 idx++;
                 if(idx ==updates.size())
                     idx = 0;
             }
+            totalTime=time;
+        }
+    }
+
+    private class third implements Runnable{
+        private int numOps;
+        public third(int num)
+        {
+            numOps=num;
+        }
+        public long totalTime;
+        @Override
+        public void run() {
+            int idx = 0;
+            int idx2 = 0;
+            long startTime;
+            long endTime;
+            long time=0;
+            for(int i = 0; i< numOps; i++)
+            {
+                startTime = System.currentTimeMillis();
+                Cursor myCurs = myDataBase.rawQuery(updates.get(idx), null);
+                myCurs.moveToFirst();
+                myCurs.close();
+                endTime = System.currentTimeMillis();
+                time += (endTime-startTime);
+                idx++;
+                if(idx ==updates.size())
+                    idx = 0;
+
+                startTime = System.currentTimeMillis();
+                myCurs = myDataBase.rawQuery(selects.get(idx2), null);
+                myCurs.moveToFirst();
+                myCurs.close();
+                endTime = System.currentTimeMillis();
+                time += (endTime-startTime);
+                idx2++;
+                if(idx2 ==selects.size())
+                    idx2 = 0;
+            }
+            totalTime = time;
         }
     }
 
@@ -64,7 +131,7 @@ public final class Paralelization extends DBHelper {
 
     @Override
     protected void Tests() {
-        testMaxConnectionsCount();
+        //testMaxConnectionsCount();
         testSingleVsMultipleThreads();
     }
 
@@ -96,78 +163,119 @@ public final class Paralelization extends DBHelper {
 
     private void testSingleVsMultipleThreads()
     {
-        selects.add("SELECT * FROM songs");
-        selects.add("SELECT _id, name, length FROM songs WHERE length > 2 AND date < 12000");
-        selects.add("SELECT count(DISTINCT author) AS count FROM songs");
-        selects.add("SELECT min(date) AS newest FROM songs");
-        selects.add("SELECT author,count(length) FROM (SELECT author,length FROM songs WHERE length > 6.5) GROUP BY author");
+        selects.add("SELECT author,count(length) FROM (SELECT author,length FROM songs WHERE length > 6.5) GROUP BY author"); //pocet skladieb dlhsich ako 6.5 min pre kazdeho autora
 
 
-        updates.add("UPDATE songs SET text_of_song = 'sample text' WHERE _id = 100");
-        updates.add("UPDATE songs SET date = date+1 WHERE date = (SELECT min(date) FROM songs)");
         updates.add("UPDATE songs SET date = date+10");
         updates.add("UPDATE songs SET date = date-10");
 
-
-        textOut.append("\n\nTesting single thread\n");
-        long total=0;
         long startTime;
         long endTime;
-        String time;
+        long totalTime=0;
+        long time=0;
+
+        //
+        // SINGLE
+        //
+        textOut.append("\n\nTesting single thread\n");
         for(int x=0;x<COUNT;x++ ) {
-            startTime = System.currentTimeMillis();
+
+            time=0;
             int idxSelect = 0;
             int idxUpdate = 0;
             for (int i = 0; i < NUM_QUERIES; i++) {
-                int count;
+                startTime = System.currentTimeMillis();
                 Cursor myCurs = myDataBase.rawQuery(selects.get(idxSelect), null);
                 myCurs.moveToFirst();
                 myCurs.close();
+                endTime = System.currentTimeMillis();
+                time += (endTime-startTime);
+
+                startTime = System.currentTimeMillis();
                 myCurs = myDataBase.rawQuery(updates.get(idxUpdate), null);
                 myCurs.moveToFirst();
                 myCurs.close();
+                endTime = System.currentTimeMillis();
+                time += (endTime-startTime);
+
                 idxSelect++;
                 idxUpdate++;
-                if (idxSelect == 5)
+                if (idxSelect == selects.size())
                     idxSelect = 0;
-                if (idxUpdate == 4)
+                if (idxUpdate == updates.size())
                     idxUpdate = 0;
             }
-            endTime = System.currentTimeMillis();
-            time = "" + (endTime - startTime);
-            textOut.append("Time: " + time + "\n");
-            total += endTime-startTime;
 
+
+            textOut.append("Time: " + time  + "\n");
+
+            totalTime+= time;
             reloadDatabase();
         }
-        textOut.append("Average: " + total/COUNT + "\n");
+        textOut.append("Average: " + totalTime/COUNT + "\n");
+
+        //
+        // TWO
+        //
 
         textOut.append("\n\nTesting two threads\n");
-        total=0;
+        totalTime=0;
         for(int x=0;x<COUNT;x++ ) {
-
-            Thread firstThread = new Thread(new first());
-            Thread secondThread = new Thread(new second());
-            startTime = System.currentTimeMillis();
+            first fst= new first(NUM_QUERIES);
+            second snd = new second(NUM_QUERIES);
+            Thread firstThread = new Thread(fst);
+            Thread secondThread = new Thread(snd);
             firstThread.start();
             secondThread.start();
             try {
                 firstThread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            try {
                 secondThread.join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            endTime = System.currentTimeMillis();
-            time = "" + (endTime - startTime);
+            firstThread.interrupt();
+            secondThread.interrupt();
+
+            time = fst.totalTime + snd.totalTime;
             textOut.append("Time: " + time + "\n");
-            total += endTime-startTime;
+            totalTime += time;
             reloadDatabase();
         }
-        textOut.append("Average: " + total/COUNT + "\n");
+        textOut.append("Average: " + totalTime/COUNT + "\n");
+
+        //
+        // THREE
+        //
+
+        textOut.append("\n\nTesting three threads\n");
+        totalTime=0;
+        for(int x=0;x<COUNT;x++ ) {
+
+            first fst= new first(NUM_QUERIES*2/3);
+            second snd = new second(NUM_QUERIES*2/3);
+            third trd = new third(NUM_QUERIES/3);
+            Thread firstThread = new Thread(fst);
+            Thread secondThread = new Thread(snd);
+            Thread thirdThread = new Thread(trd);
+            time = 0;
+
+            firstThread.start();
+            secondThread.start();
+            thirdThread.start();
+            try {
+                firstThread.join();
+                secondThread.join();
+                thirdThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            time = fst.totalTime + snd.totalTime;// + trd.totalTime;
+            textOut.append("Time: " + time + "\n");
+            totalTime += fst.totalTime + snd.totalTime + trd.totalTime;
+            reloadDatabase();
+        }
+        textOut.append("Average: " + totalTime/COUNT + "\n");
 
     }
 }
